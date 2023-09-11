8b8894fcda74c69c624934f08528a9a97173aa0e
Joseph <jnovembre@faithcatholic.com>
2023-09-11 14:00:15 -0400

    early work on project 0

---
+++ project_0/University.java
@@ -0,0 +1,32 @@
+/*
+Made by Joseph Novembre
+For Linear Data Structures course
+9/11/2023
+
+Writing a program that stores and retrieves information related to universities, CRUD processees
+
+*/
+
+public class University {
+    
+    private String uniName;
+    
+
+
+    public static void main(String args[]) {
+
+        University u1 = new University("University of Toledo", "Toledo Rockets", "Toledo", "Ohio", 1872, 15000);
+        
+
+
+    }
+
+
+
+    public University (String uniName, String nickName, 
+    String uniCity, String uniState, int foundedYear, int studentBodySize)
+    
+    {
+
+    }
+}
