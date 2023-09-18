2408af8d593bcfd57e5ef124e40c79c30c61b59e
Joseph <jnovembre@faithcatholic.com>
2023-09-18 13:57:07 -0400

    more work on project 0 9-18-23

--- project_0/University.java
+++ project_0/University.java
@@ -7,26 +7,48 @@ Writing a program that stores and retrieves information related to universities,
 
 */
 
+import java.util.Scanner;
+
+
 public class University {
     
-    private String uniName;
     
+    private String uniName;
+    private String nickName;
+    private String uniCity;
+    private String uniState;
+    private int foundedYear;
+    private int studentBodySize;
 
 
     public static void main(String args[]) {
 
-        University u1 = new University("University of Toledo", "Toledo Rockets", "Toledo", "Ohio", 1872, 15000);
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.println("Welcome to this University Collection program, please type 'help' for a list of commands.");
+        String userInput = myScanner.nextLine();
+
+        if (userInput.equals("help")) 
+        System.out.println("The commands are, list, exit, add, delete, search, save, and help");
+        else;
+
         
+        University u1 = new University();
+        String userUniName = myScanner.nextLine();
+        u1.setUniName(userUniName);
+        System.out.print(u1.uniName);
 
 
     }
 
 
-
-    public University (String uniName, String nickName, 
-    String uniCity, String uniState, int foundedYear, int studentBodySize)
     
-    {
 
+    public String getUniName() {
+        return uniName;
+    }
+
+    public void setUniName(String newUniName) {
+        this.uniName = newUniName;
     }
 }
