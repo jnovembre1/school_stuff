dcc96b870333b76749290acb8390a30e6e1a5605
Joseph <jnovembre@faithcatholic.com>
2023-09-20 21:11:06 -0400

    project 0 finished and submitted

--- project_0/University.java
+++ project_0/University.java
@@ -1,7 +1,7 @@
 /*
 Made by Joseph Novembre
 For Linear Data Structures course
-9/11/2023
+9/20/2023
 
 Writing a program that stores and retrieves information related to universities, CRUD processees
 
@@ -9,12 +9,14 @@ Writing a program that stores and retrieves information related to universities,
 
 import java.util.Scanner;
 import java.util.*;
-
+import java.io.FileWriter;
+import java.io.IOException;
 public class University {
     
-    
+    //collection the objects are stored into
     public static Collection<University> col = new ArrayList<University>();
    
+    //all the variable for the class and objects
     private String uniName;
     private String nickName;
     private String uniCity;
@@ -23,6 +25,7 @@ public class University {
     private int studentBodySize;
 
 
+    //main method that introduces to program and grabs userinput so other methods can handle everything
     public static void main(String args[]) {
 
         Scanner myScanner = new Scanner(System.in);
@@ -32,7 +35,6 @@ public class University {
         {
                 String userInput = myScanner.nextLine();
                 programLoop(userInput);            
-                System.out.println(col);
         }
         
 
@@ -40,20 +42,30 @@ public class University {
 
     }
     
+    //method associated with user inputting list into program
+    public static void listCommand() {
+        for (University u : col) {
+            System.out.println(u.uniName);
+        }
+    }
+    
+    //method associated with user inputting help into program
     public static void helpCommand() {
-        System.out.println("The commands are, list, exit, add, delete, search, save, and help");
+        System.out.println("The commands are list, exit, add, delete, search, save, and help");
     }
 
+    //method associated with user inputting the exit command into program, exits program
     public static void exitCommand() {
         System.exit(0);
     }
 
+    //when called by user, allows user to add a new University object to collection
     public static void addCommand() {
         
         boolean duplicate = false;
         System.out.println("Enter the name of the University.");
         String userUniName = getInput();
-        for (University u : col)
+        for (University u : col) //this loop checks to see if university being inputted is unique or not
         {
             if (u.uniName.toLowerCase().contains(userUniName.toLowerCase())) {
             System.out.println("Error, university already in list");
@@ -76,13 +88,77 @@ public class University {
 
         System.out.println("Enter the Universitys student body size.");
         int userBodySize = getInputInt();
-        System.out.println("Inputs succesfully registered");
+        System.out.println("Inputs succesfully registered, please enter another command.");
 
         University u = new University(userUniName, userNick, userCity, userState, userYear,userBodySize);
         col.add(u);
         }
     }
 
+    //method user can call to save info of all objects in collection to a .txt file
+    public static void saveCommand() {
+        try {
+        FileWriter myFile = new FileWriter("dump.txt");
+        for (University u : col) {
+            myFile.write(u.uniName + "," + u.nickName + "," + u.uniCity + "," + u.uniState + "," + u.foundedYear
+             + "," + u.studentBodySize + "\n");
+        }
+        myFile.close();
+        System.out.println("Succesfully saved the Universities and their info to a file");
+        } catch (IOException e ) {
+            e.printStackTrace();
+            
+        }
+        
+    }
+    
+    //method user can call to delete a object from the collection
+    public static void deleteCommand() {
+        System.out.println("Enter the Universitys name or nickname you'd like to delete.");
+        String userInput = getInput();
+        
+        
+        List<University> removeList = new ArrayList<University>(); //2nd list so I can iterate and remove from array
+        //without throwing exception
+        
+        for (University u : col) {
+            if (u.uniName.toLowerCase().contains(userInput.toLowerCase())) {
+                System.out.println("Matching University found, deleting " + userInput + " from collection.");
+
+                removeList.add(u);
+             }
+             else;
+             
+
+        }
+        if (removeList.size() == 0) {
+            System.out.println("No matching University found, please input another command.");
+        }
+         col.removeAll(removeList);
+
+
+    }
+
+    //method user can call to search for specific university by name or nickname in collection
+    public static void searchCommand() {
+        String userInput = getInput();
+        boolean isFound = false;
+        for (University u : col) {
+            if (u.uniName.toLowerCase().contains(userInput.toLowerCase()) ||
+                u.nickName.toLowerCase().contains(userInput.toLowerCase())) {
+                isFound = true;
+
+             }
+             else;
+            }
+        if (isFound == true)
+        System.out.println("Matching University found.");
+        else
+        System.out.println("Matching University not found.");
+
+    }
+
+    //main program method that calls all other command methods.
     public static void programLoop(String userInput) {
         
         
@@ -92,8 +168,19 @@ public class University {
         addCommand();
         else if (userInput.equals("exit"))
         exitCommand();
+        else if (userInput.equals("save"))
+        saveCommand();
+        else if (userInput.equals("delete"))
+        deleteCommand();
+        else if (userInput.equals("search"))
+        searchCommand();
+        else if (userInput.equals("list"))
+        listCommand();
+        else
+        System.out.println("No valid input registered, please try again.");
     }
 
+    //constructor that holds and sets everything.
     public University (String uniName, String nickName, 
     String uniCity, String uniState, int foundedYear, int studentBodySize) {
         setUniName(uniName);
@@ -104,14 +191,32 @@ public class University {
         setStudentSize(studentBodySize);
     }
 
+    //everything below this point are the getters and setters, self explanatory 
+    
     public static String getInput() {
         Scanner myScanner = new Scanner(System.in);
         return myScanner.nextLine();
     }
 
     public static int getInputInt() {
-        Scanner myScanner = new Scanner(System.in);
-        return myScanner.nextInt();
+        int value = 0;
+        boolean inputValid = true;
+        
+        // while loop that catches if user trying to throw a string into an int. will make
+        //user keep trying until they input an integer
+        while (inputValid == true) {
+            try {
+                    Scanner myScanner = new Scanner(System.in);
+                    value = myScanner.nextInt();
+                    inputValid = false;
+        }
+            catch (InputMismatchException e) {
+                    System.out.println("Invalid input, please only enter numbers.");
+        }
+        }
+        return value;
+
+        
     }
     
     public String getUniName() {
