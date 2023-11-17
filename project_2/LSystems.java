672f22a0eb1b8acae9dd5c5334a515f95a1645ed
Joseph <jnovembre@faithcatholic.com>
2023-11-17 18:49:13 -0500

    added comments to p2 and adjustments

--- project_2/LSystems.java
+++ project_2/LSystems.java
@@ -1,49 +1,67 @@
+//Made by Joseph Novembre
+// 11/17/2023
+//For EECS 2500. Project 2
+
+
+
 package project_2;
 
 import java.util.*;
 
 public class LSystems {
+   
+   
+    //our data structures
     static Queue<String> queue = new LinkedList<String>();
     static HashMap<String, String> map = new HashMap<String, String>();
     static Scanner scanner = new Scanner(System.in);
 
     
 
-
+    //self explanatory, main method where we grab our inputs from user.
     public static void main(String[] args) {
 
-        System.out.println("Enter the angle of the L-System: ");
-        int degree = scanner.nextInt();
-        
-        System.out.println("Enter the starting pattern: ");
-        String startPattern = scanner.next();
-        queueBuilder(startPattern);
+            boolean error = false;
+            while (error == false) {
+                try {
+                    System.out.println("Enter the angle of the L-System: ");
+                    int degree = scanner.nextInt(); //useless
+                
+                    System.out.println("Enter the starting pattern: ");
+                    String startPattern = scanner.next();
+                    queueBuilder(startPattern);
 
-        
-        System.out.println("Enter the number of rules: ");
-        int rules = scanner.nextInt();
-        String[] rulesArray = buildRules(rules);
-        hashFiller(rulesArray);
+                
+                    System.out.println("Enter the number of rules: ");
+                    int rules = scanner.nextInt();
+                    String[] rulesArray = buildRules(rules);
+                    hashFiller(rulesArray);
 
-        
-        System.out.println("Enter the number of expansions: ");
-        int expansions = scanner.nextInt();
-       
-        while (expansions > 0) {
+                
+                    System.out.println("Enter the number of expansions: ");
+                    int expansions = scanner.nextInt();
             
-            expansions = expander(expansions);
-            printOut();
-            
+                    while (expansions > 0) {
+                    
+                        expansions = expander(expansions);
+                        printOut();
+                    }
+                }
+                catch (Exception e) {
+                    error = true;
+                    System.out.println("Input mismatch error:, make sure you're using the correct value types for each input. Try running the program again.");
+                    
+                }
+            }
         }
-        
 
 
 
         
 
     
-    }
-
+    
+    //prints out queue string by string
    public static void printOut() {
         for (String j : queue) {
             System.out.print(j);
@@ -51,7 +69,7 @@ public class LSystems {
         System.out.print("\n");
 
    }
-   
+    //big method of this program. does all the heavy lifting and logic when it comes to expanding and the like.
     public static int expander(int expansions) {
         String[] queueArray = new String[queue.size()];
         int i = 0;
@@ -64,7 +82,7 @@ public class LSystems {
 
             }
             else 
-            map.put(s,s + s);
+            map.put(s,s + s); //this is terrible for memory management but it works and saved me from having to play around with chars everywhere
             
             if (s != null) {
     
@@ -90,7 +108,7 @@ public class LSystems {
 
     
 
-    
+    //builds the rules array to be passed to hashFiller method
     public static String[] buildRules(int rules) {
         String[] rulesArray = new String[rules];
         scanner.nextLine();
@@ -101,7 +119,7 @@ public class LSystems {
         }
         return rulesArray;
     }
-
+    //fills the hash map with rules
     public static void hashFiller(String[] rulesArray) {
         
         
@@ -117,11 +135,11 @@ public class LSystems {
             
             }
             map.put(a, n);
-            map.put("-","-");
+            
         }
 
     }
-
+    // adds stuff to queue.
     public static void queueBuilder(String startPattern) {
        for (int i =0; i < startPattern.length(); i++) {
         String s = Character.toString(startPattern.charAt(i));
