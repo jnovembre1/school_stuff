50908f28a59b18cefe23b1d53606bc57b9b0fcf5
Joseph <jnovembre@faithcatholic.com>
2023-12-02 20:31:12 -0500

    fine tuning + comments, submitting

--- project_3/hashtableProject.java
+++ project_3/hashtableProject.java
@@ -1,3 +1,7 @@
+//made by Joseph Novembre
+//Project 3 for EECS2500 
+//12/02/2023
+
 package project_3;
 
 import java.io.*;
@@ -6,11 +10,11 @@ import java.util.Scanner;
 
 public class hashtableProject {
     
-    static File file = new File ("/Users/localadmin/school_code/project_3/gatsby.txt");
-    static Hashtable<Integer, String>[] table = new Hashtable[1091];
+    static File file = new File ("/Users/localadmin/school_code/project_3/gatsby.txt"); //did absolute path since relative was being dumb
+    static Hashtable<Integer, String>[] table = new Hashtable[1091];  //hashtable initalized with 1091 buckets(arrays)
 
     
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
         populateTable();
         fileReader();
         tableData();
@@ -18,13 +22,13 @@ public class hashtableProject {
     }
 
 
-    public static void populateTable() {
+    public static void populateTable() { //initalizes our buckets in hashtable
         for (int i = 0; i < table.length; i++) {
             table[i] = new Hashtable<>();
         }
     }
 
-    public static void tableData() {
+    public static void tableData() { //prints out each word in hashtable, then prints total count of unique words
         int w = 0;
         for (int i = 0; i < table.length; i++) {
             w+= table[i].size();
@@ -39,7 +43,7 @@ public class hashtableProject {
 
     }
 
-    public static void fileReader () {
+    public static void fileReader () { //populates table from .txt file
         try {
             Scanner scanner = new Scanner(
                 new BufferedReader(
